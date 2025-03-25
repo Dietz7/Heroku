ebdaa95cf7ce606649bf03a94c61beb37bbac024
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-25 12:43:32 +0100

    Refactored slider example to improve functionality

---
+++ src/main/java/com/heroku/pages/sliderExample/SliderPage.java
@@ -0,0 +1,32 @@
+package com.heroku.pages.sliderExample;
+
+import com.heroku.config.BasePage;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class SliderPage extends BasePage {
+    public SliderPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css="input[type='range']")
+    WebElement sliderContainer;
+
+    @FindBy(css = "input[type='range']")
+    WebElement sliderValue;
+    public SliderPage moveSliderToHorizontalId() {
+        pause(100);
+        new Actions(driver).dragAndDropBy(sliderContainer, 230, 0).perform();
+        return this;
+    }
+
+    public SliderPage verifySliderValue(String number) {
+        String actualValue = sliderValue.getAttribute("value");
+        System.out.println("Slider current value: " + actualValue);
+        Assertions.assertEquals(number, actualValue);
+        return this;
+    }
+}

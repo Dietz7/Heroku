570b66869f7740ee50a7db8558823339fb837e2f
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-30 17:37:18 +0200

    updated methods

---
+++ src/main/java/com/heroku/pages/elements/TextBoxPage.java
@@ -0,0 +1,10 @@
+package com.heroku.pages.elements;
+
+import com.heroku.config.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class TextBoxPage extends BasePage {
+    public TextBoxPage(WebDriver driver) {
+        super(driver);
+    }
+}

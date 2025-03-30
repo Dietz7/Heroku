2f3ed0fe0a2ae6230dc6799ee44e4626acaa8883
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-30 17:42:06 +0200

    updated HomePage

--- src/main/java/com/heroku/pages/HomePage.java
+++ src/main/java/com/heroku/pages/HomePage.java
@@ -5,7 +5,10 @@ import com.heroku.pages.alertsFrameExamples.AlertsPage;
 import com.heroku.pages.alertsFrameExamples.BrowserWindowsPage;
 import com.heroku.pages.alertsFrameExamples.IframePage;
 import com.heroku.pages.dropList.DropListPage;
-import com.heroku.pages.slider.SliderPage;
+import com.heroku.pages.elements.ButtonPage;
+import com.heroku.pages.forms.PracticeFormPage;
+import com.heroku.pages.interactions.DragandDropPage;
+import com.heroku.pages.sliderExample.SliderPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -41,16 +44,43 @@ public class HomePage extends BasePage {
         return new BrowserWindowsPage(driver);
     }
 
-    @FindBy(css="[href='/dropdown']")
+    @FindBy(css = "[href='/dropdown']")
     WebElement dropDown;
+
     public DropListPage navigateToDropDown() {
         click(dropDown);
         return new DropListPage(driver);
     }
 
-    
-
+    @FindBy(css="[href='/horizontal_slider']")
+    WebElement horizontalSlider;
     public SliderPage navigateToHorizontalSlider() {
+        click(horizontalSlider);
         return new SliderPage(driver);
     }
+    @FindBy(css="[href='/drag_and_drop']")
+    WebElement dragDrop;
+
+    public DragandDropPage navigateToDrapAndDrag() {
+        click(dragDrop);
+        return new DragandDropPage(driver);
+    }
+
+    @FindBy(css="[href='/context_menu']")
+    WebElement contextMenu;
+
+    public ButtonPage navigateToContextMenu() {
+        click(contextMenu);
+        return new ButtonPage(driver);
+    }
+
+    @FindBy(css="[href='/upload']")
+    WebElement upload;
+
+    public PracticeFormPage navigateToFileUpload() {
+        click(upload);
+        return new PracticeFormPage(driver);
+    }
+
+
 }
